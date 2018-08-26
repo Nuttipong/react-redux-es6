6f68e8261a5699a16df19d178012a5f4579b455e
nuttipongt <nuttipong.taechasanguanwong@allianz.com>
2018-08-26 21:27:36 +0700

    refact: fixed file-loader issue with bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 /* eslint-disable import/default */
 import 'babel-polyfill';
-import './styles/styles.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/toastr/build/toastr.min.css';
+import './styles/index.scss';
 import './App';
--- webpack.development.config.js
+++ webpack.development.config.js
@@ -4,39 +4,4 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'cheap-module-eval-source-map', //source-map
-    resolve: {
-        extensions: ['.js', '.map', '.jpg', '.png', '.css', '.scss']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                use: [
-                    "file-loader?name=images/[name]-[hash:base64:5].[ext]",
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            query: {
-                                mozjpeg: {
-                                    progressive: true,
-                                },
-                                gifsicle: {
-                                    interlaced: true,
-                                },
-                                optipng: {
-                                    optimizationLevel: 7,
-                                }
-                            }
-                        }
-                    }
-                ],
-                exclude: /(node_modules)/
-            },
-            {
-                test: /\.(ttf|eot|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-                use: 'file-loader?name=fonts/[name].[ext]',
-                exclude: /(node_modules)/
-            },
-        ]
-    }
 });
